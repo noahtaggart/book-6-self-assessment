c6a20b672d03c1a038b51e416d11d92eeb1ff4c2
Noah Taggart <noahtag@gmail.com>
2022-02-11 14:44:17 -0600

    working code. decent css

--- src/scripts/Letters.js
+++ src/scripts/Letters.js
@@ -1,13 +1,13 @@
-import { getFormFields, getAuthors, getRecipients, getTopics } from "./dataAccess.js"
+import { getLetters, getAuthors, getRecipients, getTopics } from "./dataAccess.js"
 
 //targets the main container in index.html
 const mainContainer = document.querySelector("#mainContainer")
 
 //function to render database letters as html
 export const Letters = () => {
-    const letters = getFormFields()
+    const letters = getLetters()
     let html = `
-        <ul>
+        <ul id="letterContainer">
             ${letters.map(convertLetterToListElement).join("")}
         </ul>
         `
@@ -16,15 +16,19 @@ export const Letters = () => {
 
 //function to stringify letters
 const convertLetterToListElement = (letter) => {
-    //match formfield.author to author.id in order to display author.email
-    //match formField.recipient to recipient.id in order to display recipient.email
+    //match letter.author to author.id in order to display author.email
+    //match letter.recipient to recipient.id in order to display recipient.email
+    //match letter.topic to topic.id in order to display the topic
+    //converts date to basic format
     const authors = getAuthors()
     const recipients = getRecipients()
     const topics = getTopics()
-    const matchAuthor = authors.find(author => author.id === letter.author)
+    const matchAuthor = authors.find(author => author.id === letter.authorId)
     const matchRecipient = recipients.find(recipient => recipient.id === letter.recipient)
     const matchTopic = topics.find(topic => topic.id === letter.topic)
-
+    let date = new Date(letter.date)
+    date = date.toLocaleDateString("en-US")
+    
     let html = `<li class="wholeLetter">
         <div class="addresing">Dear ${matchRecipient.recipientName} (${matchRecipient.recipientEmail}),</div>
 
@@ -32,7 +36,7 @@ const convertLetterToListElement = (letter) => {
 
         <div class="letterSalutation"> Sincerely, ${matchAuthor.authorName} (${matchAuthor.authorEmail}) </div>
 
-        <div class="date">Sent on ${letter.date}</div>
+        <div class="date">Sent on ${date}</div>
 
         <div class="letterTopic">${matchTopic.topicName}</div>
         
